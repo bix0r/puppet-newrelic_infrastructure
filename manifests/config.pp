99f2688d99045281122b357f7e5436e8dac27b6d
Jacob Bednarz <jacob.bednarz@gmail.com>
2016-11-21 09:02:20 +1100

    Perform validation on license keys
    You shouldn't ever have an empty license key so let's fail loudly if
    you manage to forget it.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,10 @@ class newrelic_infrastructure::config(
   $license_key = ''
 ) {
 
+  if ($license_key == '') {
+    fail('License key cannot be empty.')
+  }
+
   file { '/etc/newrelic-infra.yml':
     ensure  => file,
     content => template('newrelic_infrastructure/etc/newrelic-infra.yml.erb'),
--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe 'newrelic_infrastructure::config' do
-  it do
-    should contain_file('/etc/newrelic-infra.yml').with_content(/^license_key:\s/)
+  context 'without a license key' do
+    it { should raise_error(Puppet::ParseError, /License key cannot be empty/) }
+  end
+
+  context 'with a valid license key' do
+    let(:params) do
+      { :license_key => 'abc123' }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml').with_content(/^license_key:\s/) }
   end
 end
