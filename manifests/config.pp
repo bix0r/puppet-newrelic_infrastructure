dde92884101f686c2fd07c04fdce508f3612e887
Birgir Haraldsson <biggi@stefna.is>
2016-12-01 13:14:08 +0100

    Adding support for all agent configuration options

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,43 @@
 class newrelic_infrastructure::config(
-  $license_key = ''
+  $license_key = '',
+  $display_name = undef,
+  $proxy = undef,
+  $verbose = undef,
+  $log_file = undef,
+  $custom_attributes = undef,
 ) {
 
   if ($license_key == '') {
     fail('License key cannot be empty.')
   }
+  if ($verbose != undef) {
+    validate_integer($verbose)
+  }
+  if ($display_name != undef) {
+    validate_string($display_name)
+  }
+  if ($proxy != undef) {
+    validate_string($proxy)
+  }
+  if ($log_file != undef) {
+    validate_string($log_file)
+  }
+  if ($custom_attributes != undef) {
+    validate_hash($custom_attributes)
+  }
+
+  $hash = {
+    license_key => $license_key,
+    display_name => $display_name,
+    proxy => $proxy,
+    verbose => $verbose,
+    log_file => $log_file,
+    custom_attributes => $custom_attributes,
+  }
 
   file { '/etc/newrelic-infra.yml':
     ensure  => file,
-    content => template('newrelic_infrastructure/etc/newrelic-infra.yml.erb'),
+    # not sure why "compact" is not available here, so using select
+    content => inline_template('<%= @hash.select{|_, value| !value.nil?}.to_yaml %>'),
   }
 }
--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -7,9 +7,127 @@ describe 'newrelic_infrastructure::config' do
 
   context 'with a valid license key' do
     let(:params) do
-      { :license_key => 'abc123' }
+      {:license_key => 'abc123'}
     end
 
     it { should contain_file('/etc/newrelic-infra.yml').with_content(/^license_key:\s/) }
+  end
+
+  context 'with non-integer verbose' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :verbose => true
+      }
+    end
+    it { should raise_error(Puppet::ParseError, /validate_integer/) }
+  end
+
+  context 'with verbose' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :verbose => 1
+      }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml')
+                    .with_content(/^license_key: abc123\nverbose: 1\n/)
+    }
+  end
+
+  context 'with non-string display_name' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :display_name => true,
+      }
+    end
+    it { should raise_error(Puppet::ParseError, /is not a string/) }
+  end
+
+  context 'with display_name' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :display_name => 'test123',
+      }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml')
+                    .with_content(/^license_key: abc123\ndisplay_name: test123\n/)
+    }
+  end
+
+  context 'with non-string proxy' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :proxy => true,
+      }
+    end
+    it { should raise_error(Puppet::ParseError, /is not a string/) }
+  end
+
+  context 'with proxy' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :proxy => 'test123',
+      }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml')
+                    .with_content(/^license_key: abc123\nproxy: test123\n/)
+    }
+  end
+
+  context 'with non-string log_file' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :log_file => true,
+      }
+    end
+    it { should raise_error(Puppet::ParseError, /is not a string/) }
+  end
+
+  context 'with log_file' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :log_file => 'test123',
+      }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml')
+                    .with_content(/^license_key: abc123\nlog_file: test123\n/)
+    }
+  end
+
+  context 'with non-hash custom_attributes' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :custom_attributes => true,
+      }
+    end
+    it { should raise_error(Puppet::ParseError, /is not a Hash/) }
+  end
+
+  context 'with custom_attributes' do
+    let(:params) do
+      {
+          :license_key => 'abc123',
+          :custom_attributes => {
+              'test_key1' => 'test1',
+              'test_key2' => 'test2',
+          }
+      }
+    end
+
+    it { should contain_file('/etc/newrelic-infra.yml')
+                    .with_content(/^license_key: abc123\ncustom_attributes:\n\s+test_key1: test1\n\s+test_key2: test2\n/)
+    }
   end
 end
